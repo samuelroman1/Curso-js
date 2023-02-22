984d17fe5f567a01f4afea77322e63099f82a4c7
samuelroman1 <samroman1934@gmail.com>
2023-02-21 22:11:05 -0500

    Primer comit

---
+++ Iniciacion-tipos.js
@@ -0,0 +1,32 @@
+//Tipos primitivos
+
+//Number
+4;
+
+//string
+
+"Hola mundo";
+'Hola mundo';
+
+// Booleanos
+true;
+false;
+
+// Nulo y undefined
+
+null;
+undefined;
+
+// null, undefined, false, 0 (Todas estas son ==> Falsy)
+// el undifined es un valor que no esta definido en memoria por lo tanto no es lo mismo
+// que un valor nulo(null)
+
+console.log(null === undefined)
+
+if(true){
+    console.log("Cumple");
+    //Si cumple
+}else{
+    console.log("No cumple")
+    //Si no cumple
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+/*
+Esta es la parte intermedia de nuestro proyecto
+En esta parte realizamos los chequeos necesarios
+Por ultimo vamo a la linea de adios
+*/
+
+//Este va a hacer el mensaje que se envia para despedir
+console.log("Adios");
